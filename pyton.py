a7729fd83672801f4ddbe5ca0a3fdc17478bf02a
stepien20 <102522088+stepien20@users.noreply.github.com>
2022-06-06 13:28:04 +0200

    kod

--- pyton.py
+++ pyton.py
@@ -1,1 +1,18 @@
-
+a=float(input("podaj a: "))
+b=float(input("podaj b: "))
+def dodawanie(a,b):
+    suma=a + b
+    print ("suma wynosi",suma) 
+def odejmowanie(a,b):
+    roznica=a - b
+    print ("roznica wynosi",roznica)
+def mnozenie(a,b):
+    iloczyn=a*b
+    print("iloczyn wynosi",iloczyn)
+def dzielenie(a,b):
+    iloraz=a/b
+    print("iloraz wynosi",iloraz)
+dodawanie(a,b)
+odejmowanie(a,b)
+mnozenie(a,b)
+dzielenie(a,b)
